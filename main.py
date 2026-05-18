908ffa132af73139b922951de493beb5257512b7
krut666-mew <amantayansar07@gmail.com>
2026-05-18 18:56:03 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+import tkinter as tk
 
+class GameApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Space Survival - Скелет")
+        self.root.geometry("400x500")
+        self.root.resizable(False, False)
+
+        self.canvas = tk.Canvas(self.root, width=400, height=500, bg="#05050F")
+        self.canvas.pack()
+
+        self.canvas.create_text(
+            200,
+            250,
+            text="Окно создано. Ожидание модулей...",
+            fill="white",
+            font=("Courier", 12)
+        )
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = GameApp(root)
+    root.mainloop()
